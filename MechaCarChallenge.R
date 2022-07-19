f0db7a06d7935c4ae6a81a1544bdb17855ffada5
apple <apple@Sha.attlocal.net>
2022-07-18 22:08:58 -0400

    Adding deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -14,3 +14,14 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 # 6. Determine p-value
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar_df))
+
+# Deliverable 2:Create Visualizations for the Trip Analysis
+
+# Import and read file
+Suspension <- read.csv(file='Suspension_coil.csv',check.names=F,stringsAsFactors = F)
+
+# Total Summary Data frame
+summary_demo <- Suspension %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI))
+
+# Lot Summary
+lot_demo <- Suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI))
